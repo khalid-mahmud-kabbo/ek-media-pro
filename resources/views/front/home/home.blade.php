e4e426644cb653262ad2a9e0bc9c07c5f91da019
Khalid Mahmud <dev.khalidmahmud01@gmail.com>
2024-07-17 16:21:10 +0600

    header and slider added

--- resources/views/front/home/home.blade.php
+++ resources/views/front/home/home.blade.php
@@ -1,1 +1,35 @@
-<div class="text-center">Hello Main Content</div>
+<div class="hero-section">
+    <div class="hero-slider">
+        @foreach ($sliders as $slider)
+            <div class="signle-slide"
+                style="background-image: url('{{ asset(SliderImage() . $slider->Background_Image) }}');">
+                <div class="container">
+                    <div class="row align-items-center">
+                        <div class="col-lg-6 col-6">
+                            <div class="hero-slider-content text-center">
+                                <h2 class="slider-sub-title">
+                                    {{ langConverter($slider->en_Sub_Title, $slider->fr_Sub_Title) }}</h2>
+                                <h1 class="slider-title">
+                                    {{ langConverter($slider->en_Title, $slider->fr_Title) }}
+                                </h1>
+                                <p class="slider-text">
+                                    {{ langConverter($slider->en_Description, $slider->fr_Description) }}</p>
+                                <div class="slider-btn">
+                                    <a href="{{ route('all.product') }}"
+                                        class="secondary-btn">{{ langConverter($slider->en_Button_Text, $slider->fr_Button_Text) }}
+                                        <i class="iocn flaticon-right-arrow"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-5 offset-lg-1 col-6">
+                            <div class="hero-slider-image text-center">
+                                <img class="hero-image img-fluid" src="{{ asset(SliderImage() . $slider->Thumbnail) }}"
+                                    alt="hero-banner-image" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        @endforeach
+    </div>
+</div>
--- resources/views/front/layouts/include/header.blade.php
+++ resources/views/front/layouts/include/header.blade.php
@@ -1,3 +1,74 @@
-<header class="for-desktop">
-    <div class="text-center">Hello Iam Header</div>
+<header class="for-desktop header-area">
+   
+    <div class="header-middle">
+        <div class="container">
+            <div class="header-middle-wrap">
+                <div class="brand-area">
+                    <a class="brand-logo" href="{{ route('front') }}"><img class="brand-image"
+                            src="{{ asset(IMG_LOGO_PATH . $allsettings['main_logo']) }}"
+                            alt="{{ $allsettings['app_title'] }}" /></a>
+                </div>
+                <div class="search-area">
+                    <form action="{{ route('category.search') }}" method="get">
+                        <div class="search-wrap">
+                            <div class="form-group">
+                                <input type="text" class="form-control" id="search" name="search"
+                                    placeholder="{{ __('Search Here') }}" />
+                                <button type="submit" class="search-btn"><i
+                                        class="flaticon-search"></i></button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+
+                <div class="header-right">
+
+                    <div class="wishlist single-btn">
+                        <a href="{{ route('wishlist') }}" class="wishlist-btn header-btn">
+                            <div class="btn-left">
+                                <i class="btn-icon flaticon-like"></i>
+                                <span
+                                    class="count wishListCuntFromController">{{ auth()->check() ? wishlistCount() : '0' }}</span>
+                            </div>
+                            <div class="btn-right">
+                                <span class="btn-text">{{ __('Wishlist') }}</span>
+                                <span
+                                    class="item-count wishListCuntFromController">{{ auth()->check() ? wishlistCount() : '0' }}
+                                    {{ __('items') }}</span>
+                            </div>
+                        </a>
+                    </div>
+
+
+                    <div class="cart single-btn">
+                        <a data-bs-toggle="offcanvas" href="#cartOffcanvasSidebar" role="button"
+                            aria-controls="cartOffcanvasSidebar" class="cart-btn header-btn">
+                            <div class="btn-left">
+                                <i class="btn-icon flaticon-shopping-bag"></i>
+                                <span class="count totalCountItem">{{ cartCountItem() }}</span>
+                            </div>
+                            <div class="btn-right">
+                                <span class="btn-text">{{ __('Your Cart') }}</span>
+                                @php
+                                    $content = Cart::content();
+                                    $total = 0;
+                                @endphp
+                                @foreach ($content as $item)
+                                    @php
+                                        $total += $item->subtotal;
+                                    @endphp
+                                @endforeach
+                                <span class="price totalAmount">
+                                    {{ currencyConverter($total) }}</span>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+
 </header>
