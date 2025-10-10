009c7bf5ccdd0fcf589275cb24344ef0cd68ac2a
Khalid Mahmud <dev.khalidmahmud01@gmail.com>
2025-10-10 19:26:03 +0600

    docs one more updated

--- resources/views/frontend/layout/includes/docs/scripts.blade.php
+++ resources/views/frontend/layout/includes/docs/scripts.blade.php
@@ -185,6 +185,15 @@
 </script>
 
 
+    <script>
+        $(document).on("click", ".btn-copy-api-key", function () {
+            $("#api-key").select();
+            document.execCommand("copy");
+            $('#copy-api-key-text').text('Copied!');
+        });
+    </script>
+
+
 <script defer src="https://static.cloudflareinsights.com/beacon.min.js/vcd15cbe7772f49c399c6a5babf22c1241717689176015" integrity="sha512-ZpsOmlRQV6y907TI0dKBHq9Md29nnaEIPlkf84rnaERnq6zvWvPUqr2ft8M1aS28oN72PdrCzSjY4U6VaAw1EQ==" data-cf-beacon='{"version":"2024.11.0","token":"2a196c9d61154b7091d345f45de42947","server_timing":{"name":{"cfCacheStatus":true,"cfEdge":true,"cfExtPri":true,"cfL4":true,"cfOrigin":true,"cfSpeedBrain":true},"location_startswith":null}}' crossorigin="anonymous"></script>
 
 @stack('post_scripts')
